d82a4e0a2f1a0062c6484f9fea2c57570ea44544
Pranay Lendave <71246491+PranayLendave@users.noreply.github.com>
2023-09-02 17:46:41 +0530

    Update train.py

--- train.py
+++ train.py
@@ -36,6 +36,8 @@ class NeRFSystem(LightningModule):
         self.embedding_dir = Embedding(3, 4) # 4 is the default number
         self.embeddings = [self.embedding_xyz, self.embedding_dir]
 
+        self.validation_step_outputs = []
+
         self.nerf_coarse = NeRF()
         self.models = [self.nerf_coarse]
         if hparams_1.N_importance > 0:
@@ -117,30 +119,125 @@ class NeRFSystem(LightningModule):
                 'log': log
                }
 
+    # def validation_step(self, batch, batch_nb):
+    #     rays, rgbs = self.decode_batch(batch)
+    #     rays = rays.squeeze() # (H*W, 3)
+    #     rgbs = rgbs.squeeze() # (H*W, 3)
+    #     results = self(rays)
+    #     log = {'val_loss': self.loss(results, rgbs)}
+    #     typ = 'fine' if 'rgb_fine' in results else 'coarse'
+    
+    #     if batch_nb == 0:
+    #         W, H = self.hparams_1.img_wh
+    #         img = results[f'rgb_{typ}'].view(H, W, 3).cpu()
+    #         img = img.permute(2, 0, 1) # (3, H, W)
+    #         img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu() # (3, H, W)
+    #         depth = visualize_depth(results[f'depth_{typ}'].view(H, W)) # (3, H, W)
+    #         stack = torch.stack([img_gt, img, depth]) # (3, 3, H, W)
+    #         self.logger.experiment.add_images('val/GT_pred_depth',
+    #                                            stack, self.global_step)
+
+    #     log['val_psnr'] = psnr(results[f'rgb_{typ}'], rgbs)
+
+    #     return log
+
     def validation_step(self, batch, batch_nb):
         rays, rgbs = self.decode_batch(batch)
-        rays = rays.squeeze() # (H*W, 3)
-        rgbs = rgbs.squeeze() # (H*W, 3)
+        rays = rays.squeeze()
+        rgbs = rgbs.squeeze()
         results = self(rays)
-        log = {'val_loss': self.loss(results, rgbs)}
+
+        # Compute validation loss (comparison between predicted and ground-truth RGB values)
+        val_loss = self.loss(results, rgbs)
+        
         typ = 'fine' if 'rgb_fine' in results else 'coarse'
-    
+
         if batch_nb == 0:
             W, H = self.hparams_1.img_wh
             img = results[f'rgb_{typ}'].view(H, W, 3).cpu()
-            img = img.permute(2, 0, 1) # (3, H, W)
-            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu() # (3, H, W)
-            depth = visualize_depth(results[f'depth_{typ}'].view(H, W)) # (3, H, W)
-            stack = torch.stack([img_gt, img, depth]) # (3, 3, H, W)
-            self.logger.experiment.add_images('val/GT_pred_depth',
-                                               stack, self.global_step)
+            img = img.permute(2, 0, 1)
+            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu()
+            depth = visualize_depth(results[f'depth_{typ}'].view(H, W))
 
-        log['val_psnr'] = psnr(results[f'rgb_{typ}'], rgbs)
+            # stack = torch.stack([img_gt, img, depth])
+            # self.logger.experiment.add_images('val/GT_pred_depth', stack, self.global_step)
+
+            print("\n")
+            print("img_gt", img_gt.shape, img_gt.dtype, img_gt.device)  # Print image shape and data type
+            print("img", img.shape, img.dtype)
+            print("depth", depth.shape, depth.dtype)
+
+            # img_gt_pil = Image.fromarray(img_gt.permute(1, 2, 0).numpy())
+            # img_pil = Image.fromarray(img.permute(1, 2, 0).numpy())
+            # depth_pil = Image.fromarray(depth.permute(1, 2, 0).numpy())
+
+            # Log images using the WandbLogger
+            # self.logger.log_images(
+            #     "val/GT_pred_depth",
+            #     images=[img_gt_pil, img_pil, depth_pil],
+            #     step=self.global_step
+            # )
+            img_gt_np = img_gt.cpu().numpy().astype(np.uint8)  # Change the data type if needed
+            img_np = img.cpu().numpy().astype(np.uint8)  # Change the data type if needed
+            depth_np = depth.cpu().numpy().astype(np.uint8)  # Change the data type if needed
+
+            # Log images with captions (optional)
+            captions = ["Ground Truth", "Predicted", "Depth"]
+            images = [img_gt_np, img_np, depth_np]
+
+            # Log the images with captions
+            wandb.log({f"val/GT_pred_depth_{i}": [wandb.Image(image, caption=caption) for i, (image, caption) in enumerate(zip(images, captions))]}, step=self.global_step)
+            self.logger.log_images(
+                                    "val/GT_pred_depth",
+                                    images=images,
+                                    caption=captions,
+                                    step=self.global_step
+                                )
+
+
+        # Compute Peak Signal-to-Noise Ratio (PSNR) between predicted and ground-truth RGB values
+        val_psnr = psnr(results[f'rgb_{typ}'], rgbs)
+
+        # Return both validation loss and PSNR as part of the log
+        log = {'val_loss': val_loss, 'val_psnr': val_psnr}
+
+        # Append the validation step output to the list
+        self.validation_step_outputs.append(log)
+
         return log
 
-    def validation_epoch_end(self, outputs):
-        mean_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        mean_psnr = torch.stack([x['val_psnr'] for x in outputs]).mean()
+
+    # def validation_epoch_end(self, outputs):
+    #     mean_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+    #     mean_psnr = torch.stack([x['val_psnr'] for x in outputs]).mean()
+
+    #     return {'progress_bar': {'val_loss': mean_loss,
+    #                              'val_psnr': mean_psnr},
+    #             'log': {'val/loss': mean_loss,
+    #                     'val/psnr': mean_psnr}
+    #            }
+
+    def on_validation_epoch_end(self):
+        # Initialize lists to store validation losses and PSNRs
+        validation_losses = []
+        validation_psnrs = []
+
+        # Iterate over the validation step outputs to collect losses and PSNRs
+        for output in self.validation_step_outputs:
+            validation_losses.append(output['val_loss'])
+            validation_psnrs.append(output['val_psnr'])
+
+        # Calculate the mean validation loss and mean PSNR
+        if len(validation_losses) > 0:
+            mean_loss = torch.stack(validation_losses).mean()
+            mean_psnr = torch.stack(validation_psnrs).mean()
+
+            # Log the mean validation loss and mean PSNR
+            self.log("val_loss", mean_loss)
+            self.log("val_psnr", mean_psnr)
+
+        # Clear the lists to free memory
+        self.validation_step_outputs.clear()
 
         return {'progress_bar': {'val_loss': mean_loss,
                                  'val_psnr': mean_psnr},
@@ -158,12 +255,12 @@ if __name__ == '__main__':
     #                                       mode='min',
     #                                       save_top_k=5,)
     checkpoint_callback = ModelCheckpoint(
-                                                                                dirpath=os.path.join(f'ckpts/{hparams_1.exp_name}'),
-                                                                                filename='{epoch:d}',
-                                                                                monitor='val/loss',
-                                                                                mode='min',
-                                                                                save_top_k=5,
-                                                                                )
+                                            dirpath=os.path.join(f'ckpts/{hparams_1.exp_name}'),
+                                            filename='{epoch:d}',
+                                            monitor='val/loss',
+                                            mode='min',
+                                            save_top_k=5,
+                                                        )
 
 
     logger = WandbLogger()
@@ -190,10 +287,11 @@ if __name__ == '__main__':
                         enable_checkpointing=True,
                         enable_progress_bar=True,
                         devices=hparams_1.num_gpus,
-                        strategy='ddp' if hparams_1.num_gpus>1 else None,
+                        # strategy='ddp' if hparams_1.num_gpus>1 else None,
+                        strategy='auto',
                         num_sanity_val_steps=1,
                         benchmark=True,
-                        profiler=hparams_1.num_gpus==1
+                        # profiler=hparams_1.num_gpus==1
                     )
 
     trainer.fit(system)
